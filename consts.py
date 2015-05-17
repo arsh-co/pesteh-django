fc5d0b2dbb9401cdf3f818685ae3b7bad8e84ee1
mmmdamin <mmmdamin@gmail.com>
2015-05-17 17:39:27 +0430

    register user, register device, send message

--- consts.py
+++ consts.py
@@ -6,5 +6,5 @@ PESTEH_APPLICATIONS_URL = u"{}applications/".format(PESTEH_URL)
 REGISTER_DEVICE_URL = u"{}register-device/".format(PESTEH_APPLICATIONS_URL)
 REGISTER_USER_URL = u"{}register-user/".format(PESTEH_APPLICATIONS_URL)
 
-PESTEH_MESSAGES_URL = u"{}messages/"
+PESTEH_MESSAGES_URL = u"{}messages/".format(PESTEH_URL)
 SEND_MESSAGE_URL = u"{}send/".format(PESTEH_MESSAGES_URL)
--- pesteh.py
+++ pesteh.py
@@ -1,5 +1,21 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
+
+
+def send_request(url, data, use_client_id=True, use_client_secret=True):
+    import json
+    import httplib2
+
+    if use_client_id:
+        data.update({'client_id': settings.PESTEH_CLIENT_ID})
+    if use_client_secret:
+        data.update({'client_secret': settings.PESTEH_CLIENT_SECRET})
+    params = json.dumps(data)
+    h = httplib2.Http()
+    resp, content = h.request(url, body=params, method="POST")
+    return json.loads(content)
+
 
 def generate_user_token():
     import string
