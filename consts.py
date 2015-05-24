d4827f4a76569123c852d3b650db8bb64bbf8e38
mmmdamin <mmmdamin@gmail.com>
2015-05-24 11:18:40 +0430

    server: http://cloud.arsh.co/

--- consts.py
+++ consts.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-PESTEH_URL = u"http://localhost:8008/pesteh/"
+PESTEH_URL = u"http://cloud.arsh.co/pesteh/"
 
 REGISTER_DEVICE_URL = u"{}devices/register".format(PESTEH_URL)
 SEND_MESSAGE_URL = u"{}send".format(PESTEH_URL)
