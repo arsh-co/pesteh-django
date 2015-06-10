0ee1e4d9a25c919f046bff3b689495349f44ad4d
mmmdamin <mmmdamin@gmail.com>
2015-06-10 16:46:50 +0430

    change server url to https

--- consts.py
+++ consts.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-PESTEH_URL = u"http://cloud.arsh.co/pesteh/"
+PESTEH_URL = u"https://cloud.arsh.co/pesteh/"
 
 REGISTER_DEVICE_URL = u"{}devices/register".format(PESTEH_URL)
 SEND_MESSAGE_URL = u"{}send".format(PESTEH_URL)
