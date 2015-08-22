d669cea73edd2aee7982798b1ce4ada4f8dab2fd
MohammadAmin Sabbaghian <sabbaghian@arsh.co>
2015-08-22 11:38:43 +0430

    use requests instead of httplib2

--- pesteh.py
+++ pesteh.py
@@ -1,20 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from django.conf import settings
+import requests
 
 
 def send_request(url, data, use_client_id=True, use_client_secret=True):
     import json
-    import httplib2
 
     if use_client_id:
         data.update({'client_id': settings.PESTEH_CLIENT_ID})
     if use_client_secret:
         data.update({'client_secret': settings.PESTEH_CLIENT_SECRET})
     params = json.dumps(data)
-    h = httplib2.Http(".cache", disable_ssl_certificate_validation=True)
-    resp, content = h.request(url, body=params, method="POST", headers={u"content-type": 'application/json'})
-    return json.loads(content)
+    resp = requests.post(url, data=params, headers={u"content-type": 'application/json'})
+    return json.loads(resp.content)
 
 
 def generate_user_token(user):
