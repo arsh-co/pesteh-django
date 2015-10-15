430004d03c104dd8c478f3bbcf90f2e1472c37e4
MohammadAmin Sabbaghian <sabbaghian@arsh.co>
2015-10-15 13:12:36 +0330

    retry for device register

--- consts.py
+++ consts.py
@@ -4,3 +4,5 @@ PESTEH_URL = u"https://cloud.arsh.co/pesteh/"
 
 REGISTER_DEVICE_URL = u"{}devices/register".format(PESTEH_URL)
 SEND_MESSAGE_URL = u"{}send".format(PESTEH_URL)
+
+MAX_RETRIES = 50
--- pesteh.py
+++ pesteh.py
@@ -2,8 +2,11 @@
 
 from django.conf import settings
 import requests
+from requests.packages.urllib3 import Retry
 from requests.packages.urllib3.connection import ConnectionError
 import time
+
+from pesteh.consts import MAX_RETRIES
 
 
 def send_request(url, data, use_client_id=True, use_client_secret=True):
@@ -16,10 +19,14 @@ def send_request(url, data, use_client_id=True, use_client_secret=True):
     params = json.dumps(data)
 
     c = 0
+    s = requests.Session()
+    https_retries = Retry(total=MAX_RETRIES)
+    https = requests.adapters.HTTPAdapter(max_retries=https_retries)
+    s.mount('https://', https)
     while c < 100:
         c += 1
         try:
-            resp = requests.post(url, data=params, headers={u"content-type": 'application/json'})
+            resp = s.post(url, data=params, headers={u"content-type": 'application/json'})
         except ConnectionError:
             time.sleep(0.05)
         else:
